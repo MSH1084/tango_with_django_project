f05dc21add9b91aa68ea97e2bc8edcc8a1670fe5
“Mohamed <2833753s@student.gla.ac.uk>
2024-02-09 18:24:29 +0000

    add login

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),

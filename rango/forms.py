5aa1b8c8dae89cbca71690592a270d7b6ff95dc0
“Mohamed <2833753s@student.gla.ac.uk>
2024-02-09 18:14:06 +0000

    added registration

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile
+from django.contrib.auth.models import User
+
 
 
 
@@ -40,6 +42,16 @@ class PageForm(forms.ModelForm):
                 url = f'http://{url}'
                 cleaned_data['url'] = url
             return cleaned_data
+    class UserForm(forms.ModelForm):
+        password = forms.CharField(widget=forms.PasswordInput())
+        class Meta: 
+            model = User
+            fields = ('username', 'email', 'password',)
+    class UserProfileForm(forms.ModelForm): 
+        class Meta:
+            model = UserProfile
+            fields = ('website', 'picture',)
+
     # What fields do we want to include in our form?
     # This way we don't need every field in the model present.
     # Some fields may allow NULL values; we may not want to include them. # Here, we are hiding the foreign key.
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rango import views
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
-from rango.forms import PageForm
+
 # Create your views here.
 
 
@@ -98,4 +97,41 @@ def add_page(request, category_name_slug):
         else: 
             print(form.errors)
     context_dict = {'form': form, 'category': category}
-    return render(request, 'rango/add_page.html', context=context_dict)        
+    return render(request, 'rango/add_page.html', context=context_dict)     
+  
+def register(request):
+# A boolean value for telling the template
+# whether the registration was successful.
+# Set to False initially. Code changes value to # True when registration succeeds.
+    registered = False
+    # If it's a HTTP POST, we're interested in processing form data.
+    if request.method == 'POST':
+# Attempt to grab information from the raw form information. # Note that we make use of both UserForm and UserProfileForm. 
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+        # If the two forms are valid...
+        if user_form.is_valid() and profile_form.is_valid(): # Save the user's form data to the database. 
+            user = user_form.save()
+# Now we hash the password with the set_password method. # Once hashed, we can update the user object. 
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+             # Did the user provide a profile picture?
+               # If so, we need to get it from the input form and #put it in the UserProfile model.
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html',
+                  context = {'user_form': user_form,
+                             'profile_form': profile_form,
+                             'registered': registered})
